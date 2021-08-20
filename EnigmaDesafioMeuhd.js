a978ca50e9839c4bec255d273800586f283ce357
meuhd <noirane.se@gmail.com>
2021-08-20 14:52:50 -0300

    Primeira Versão

---
+++ EnigmaDesafioMeuhd.js
@@ -0,0 +1,11 @@
+function TestaChave(){
+    var inSenha = document.getElementById("inChave");
+    var senha = inSenha.value;
+    if (senha=="23/06/1912"){
+        alert ("Chave correta !"); // chamar outra pagina, ou alerta ou codigo
+    } else{
+        alert ("Chave incorreta !");
+    }
+
+}
+
